a1fdb73e14c907ddabd31f3afeadef3076865fa8
Yash Anand Khade <101806338+yashakhade@users.noreply.github.com>
2025-03-06 19:00:33 +0530

    mac 1

--- lib/food_reciept/Views/home_screen_recipe.dart
+++ lib/food_reciept/Views/home_screen_recipe.dart
@@ -20,109 +20,228 @@ class _HomeScreenRecipeState extends State<HomeScreenRecipe> {
     return Scaffold(
       backgroundColor: Colors.grey.shade200,
       body: SafeArea(
-          child: Column(
-        children: [
-          headerParts(),
-          30.h,
-          mySearchField(),
-          40.h,
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "Popular Menus",
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
-                ),
-                Text(
-                  "See All",
-                  style: TextStyle(
-                      fontWeight: FontWeight.w500,
-                      fontSize: 14,
-                      color: Colors.green),
-                )
-              ],
-            ),
-          ),
-          20.h,
-          // SingleChildScrollView(
-          //   scrollDirection: Axis.horizontal,
-          //   child: Row(
-          //     children: List.generate(
-          //       menuItems.length,
-          //       (index) => Padding(
-          //         padding: EdgeInsets.symmetric(horizontal: 10),
-          //         child: GestureDetector(
-          //           onTap: () {
-          //             selectedIndex = index;
-          //             setState(() {
-          //
-          //             });
-          //           },
-          //           child: Container(
-          //             padding:
-          //                 EdgeInsets.symmetric(horizontal: 15, vertical: 7),
-          //             decoration: BoxDecoration(
-          //               borderRadius: BorderRadius.circular(20),
-          //               gradient: LinearGradient(
-          //                 colors: [
-          //                   selectedIndex == index
-          //                       ? Colors.green
-          //                       : Colors.white,
-          //                   selectedIndex == index
-          //                       ? Colors.greenAccent
-          //                       : Colors.white
-          //                 ],
-          //               ),
-          //             ),
-          //             child: Text(
-          //               menuItems[index],
-          //               style: TextStyle(
-          //                   fontSize: 15,
-          //                   fontWeight: selectedIndex == index
-          //                       ? FontWeight.bold
-          //                       : FontWeight.w500,
-          //                   color: selectedIndex == index
-          //                       ? Colors.white
-          //                       : Colors.black),
-          //             ),
-          //           ),
-          //         ),
-          //       ),
-          //     ),
-          //   ),
-          // )
-
-          popularMenuItems(),
-          20.h,
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: List.generate(
-                recipeItems.length,
-                (index) {
-                  final recipe = recipeItems[index];
-                  return Padding(
-                    padding: index == 0
-                        ? EdgeInsets.only(left: 20, right: 10)
-                        : EdgeInsets.only(right: 10),
-                    child: GestureDetector(
-                      onTap: () {},
-                      child: Container(
-                        height: 260,
-                        width: _size.width / 2.45,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(8),
-                            image: DecorationImage(image: AssetImage(recipe.image),fit: BoxFit.cover)),
-                      ),
-                    ),
-                  );
-                },
+          child: SingleChildScrollView(
+        child: Column(
+          children: [
+            headerParts(),
+            30.h,
+            mySearchField(),
+            40.h,
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Popular Menus",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+                  ),
+                  Text(
+                    "See All",
+                    style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 14,
+                        color: Colors.green),
+                  )
+                ],
               ),
             ),
-          )
-        ],
+            20.h,
+            // SingleChildScrollView(
+            //   scrollDirection: Axis.horizontal,
+            //   child: Row(
+            //     children: List.generate(
+            //       menuItems.length,
+            //       (index) => Padding(
+            //         padding: EdgeInsets.symmetric(horizontal: 10),
+            //         child: GestureDetector(
+            //           onTap: () {
+            //             selectedIndex = index;
+            //             setState(() {
+            //
+            //             });
+            //           },
+            //           child: Container(
+            //             padding:
+            //                 EdgeInsets.symmetric(horizontal: 15, vertical: 7),
+            //             decoration: BoxDecoration(
+            //               borderRadius: BorderRadius.circular(20),
+            //               gradient: LinearGradient(
+            //                 colors: [
+            //                   selectedIndex == index
+            //                       ? Colors.green
+            //                       : Colors.white,
+            //                   selectedIndex == index
+            //                       ? Colors.greenAccent
+            //                       : Colors.white
+            //                 ],
+            //               ),
+            //             ),
+            //             child: Text(
+            //               menuItems[index],
+            //               style: TextStyle(
+            //                   fontSize: 15,
+            //                   fontWeight: selectedIndex == index
+            //                       ? FontWeight.bold
+            //                       : FontWeight.w500,
+            //                   color: selectedIndex == index
+            //                       ? Colors.white
+            //                       : Colors.black),
+            //             ),
+            //           ),
+            //         ),
+            //       ),
+            //     ),
+            //   ),
+            // )
+
+            popularMenuItems(),
+            20.h,
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: List.generate(
+                  recipeItems.length,
+                  (index) {
+                    final recipe = recipeItems[index];
+                    return Padding(
+                      padding: index == 0
+                          ? EdgeInsets.only(left: 20, right: 10)
+                          : EdgeInsets.only(right: 10),
+                      child: GestureDetector(
+                        onTap: () {},
+                        child: Container(
+                          height: 260,
+                          width: _size.width / 2.45,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8),
+                            image: DecorationImage(
+                                image: AssetImage(recipe.image),
+                                fit: BoxFit.cover),
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              Container(
+                                padding: EdgeInsets.all(5),
+                                margin: EdgeInsets.all(5),
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color:
+                                      recipe.fav ? Colors.red : Colors.black45,
+                                ),
+                                child: Icon(
+                                  Iconsax.heart,
+                                  color: Colors.white,
+                                  size: 18,
+                                ),
+                              ),
+                              Container(
+                                padding: EdgeInsets.all(10),
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10),
+                                    color: Colors.black45),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Expanded(
+                                          child: Text(
+                                            recipe.name,
+                                            maxLines: 1,
+                                            style: TextStyle(
+                                                fontSize: 16,
+                                                height: 0,
+                                                color: Colors.white,
+                                                fontWeight: FontWeight.w500),
+                                          ),
+                                        ),
+                                        Row(
+                                          children: [
+                                            Icon(
+                                              Icons.star,
+                                              color: Colors.yellow,
+                                              size: 18,
+                                            ),
+                                            Text(
+                                              "${recipe.rate}",
+                                              style: TextStyle(
+                                                  height: 0,
+                                                  color: Colors.white),
+                                            )
+                                          ],
+                                        )
+                                      ],
+                                    ),
+                                    3.h,
+                                    Text(
+                                      "1 Bowl (${recipe.weight}g)",
+                                      style: TextStyle(
+                                          color: Colors.white70,
+                                          fontSize: 12,
+                                          height: 0),
+                                    ),
+                                    3.h,
+                                    Text(
+                                      "${recipe.calorie} Kkal | 25% AKI",
+                                      style: TextStyle(color: Colors.white70),
+                                      maxLines: 1,
+                                      overflow: TextOverflow.ellipsis,
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+            40.h,
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Categories",
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    "See All",
+                    style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        color: Colors.green,
+                        fontSize: 14),
+                  )
+                ],
+              ),
+            ),
+            10.h,
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: List.generate(
+                  10,
+                  (index) {
+                    return Padding(
+                      padding:index == 0? EdgeInsets.only(left: 20,right: 10):EdgeInsets.only(right: 10),
+                      child: Container(height: 100,width: 100,decoration: BoxDecoration(color: Colors.yellow),),
+                    );
+                  },
+                ),
+              ),
+            )
+          ],
+        ),
       )),
     );
   }
